b4de8d7b10250b4a412efeb58588db18ab22b5be
JohnGallion <jlwgallion@gmail.com>
2023-04-22 14:02:03 -0400

    input field on page

--- src/App.js
+++ src/App.js
@@ -3,10 +3,15 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-      <h1>Welcome to Weather Guru</h1>
+    <div  className="App">
+      <header >
+      <h1 class="text-2xl">Welcome to Weather Guru</h1>
       </header>
+      <div >
+        <input   type="text" placeholder='Search by City'></input>
+        <button  >Get Weather</button>
+      </div>
+
     </div>
   );
 }
